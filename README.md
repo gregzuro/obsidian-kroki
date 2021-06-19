8d966795a4fa92cc1481e9c1776b35a290731c19
Greg Zuro <greg@zuro.net>
2021-06-18 18:28:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Obsidian PlantUML
+
+Render [PlantUML](https://plantuml.com) Diagrams in [Obsidian](https://obsidian.md)
+
+![Demonstration](https://i.imgur.com/Ueyw0SE.gif)
+
+This plugin uses the [PlantUML Online Server](https://plantuml.com/server) for rendering.
+
+You can also host your own server
+([Docker](https://hub.docker.com/r/plantuml/plantuml-server) /
+[JEE](https://plantuml.com/de/server) /
+[PicoWeb](https://plantuml.com/de/picoweb)) and specify its address in the settings.
+
+## Usage
+Create a fenced codeblock using `plantuml` as the language.
+Specify your plantuml code inside.
+
+You can also use `plantuml-ascii` to generate ASCII Art, or `plantuml-map` to generate a png image
+with clickable links.
+
+Documentation on Plantuml can be found on [plantuml.com](https://plantuml.com/)
+
+
+## Examples
+
+```yaml
+    ```plantuml
+    Bob -> Alice : hello
+    Alice -> Wonderland: hello
+    Wonderland -> next: hello
+    next -> Last: hello
+    Last -> next: hello
+    next -> Wonderland : hello
+    Wonderland -> Alice : hello
+    Alice -> Bob: hello
+    ```
+```
+results in:
+
+![](http://www.plantuml.com/plantuml/png/SyfFEhH0r-xG0iUSpEJKGmki3Yt8ICt9oUS2yo5IuVbvAQb5EObvAN1PX114ILvgHGbSKW48G08GAP_4ObGfa011NSWMe2X1IA2x6w46oUr0_y6a0000)
+
+```yaml
+    ```plantuml-ascii
+    Bob -> Alice : hello
+    Alice -> Wonderland: hello
+    Wonderland -> next: hello
+    next -> Last: hello
+    Last -> next: hello
+    next -> Wonderland : hello
+    Wonderland -> Alice : hello
+    Alice -> Bob: hello
+    ```
+```
+results in:
+```
+     ┌───┐          ┌─────┐          ┌──────────┐          ┌────┐          ┌────┐
+     │Bob│          │Alice│          │Wonderland│          │next│          │Last│
+     └─┬─┘          └──┬──┘          └────┬─────┘          └─┬──┘          └─┬──┘
+       │    hello      │                  │                  │               │   
+       │──────────────>│                  │                  │               │   
+       │               │                  │                  │               │   
+       │               │      hello       │                  │               │   
+       │               │─────────────────>│                  │               │   
+       │               │                  │                  │               │   
+       │               │                  │       hello      │               │   
+       │               │                  │ ─────────────────>               │   
+       │               │                  │                  │               │   
+       │               │                  │                  │     hello     │   
+       │               │                  │                  │ ──────────────>   
+       │               │                  │                  │               │   
+       │               │                  │                  │     hello     │   
+       │               │                  │                  │ <──────────────   
+       │               │                  │                  │               │   
+       │               │                  │       hello      │               │   
+       │               │                  │ <─────────────────               │   
+       │               │                  │                  │               │   
+       │               │      hello       │                  │               │   
+       │               │<─────────────────│                  │               │   
+       │               │                  │                  │               │   
+       │    hello      │                  │                  │               │   
+       │<──────────────│                  │                  │               │   
+     ┌─┴─┐          ┌──┴──┐          ┌────┴─────┐          ┌─┴──┐          ┌─┴──┐
+     │Bob│          │Alice│          │Wonderland│          │next│          │Last│
+     └───┘          └─────┘          └──────────┘          └────┘          └────┘
+```
+
+
+## Installation
+
+### Inside Obsidian
+
+`Settings > Third-party plugins > Community Plugins > Browse` and search for `PlantUML`.
+
+### Manually installing the plugin
+
+- Clone this repo
+- `npm i` or `yarn` to install dependencies
+- `npm run build`  
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/obsidian-plantuml/`.
---
+++ main.ts
@@ -0,0 +1,135 @@
+import {App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting} from 'obsidian';
+
+import * as plantuml from 'plantuml-encoder'
+
+interface PlantUMLSettings {
+    server_url: string,
+    header: string;
+}
+
+const DEFAULT_SETTINGS: PlantUMLSettings = {
+    server_url: 'https://www.plantuml.com/plantuml',
+    header: ''
+}
+
+export default class PlantumlPlugin extends Plugin {
+    settings: PlantUMLSettings;
+
+    pngProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        const dest = document.createElement('img');
+        const prefix = this.settings.server_url + "/png/";
+        source = source.replace(/&nbsp;/gi, " ");
+
+        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
+
+        dest.src = prefix + encoded;
+
+        el.appendChild(dest);
+    };
+
+    mapProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        const dest = document.createElement('div');
+        let prefix = this.settings.server_url + "/png/";
+        source = source.replace(/&nbsp;/gi, " ");
+
+        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
+
+        const img = document.createElement("img");
+        img.src = prefix + encoded;
+        img.useMap = "#" + encoded;
+
+        prefix = this.settings.server_url + "/map/";
+
+        const result = await fetch(prefix + encoded, {
+            method: "GET"
+        });
+
+        if(result.ok) {
+            dest.innerHTML = await result.text();
+            dest.children[0].setAttr("name", encoded);
+        }
+        dest.appendChild(img);
+        el.appendChild(dest);
+    };
+
+    asciiProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        const prefix = this.settings.server_url + "/txt/";
+        source = source.replace(/&nbsp;/gi, " ");
+
+        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
+        const result = await fetch(prefix + encoded, {
+            method: "GET"
+        });
+
+        if(result.ok) {
+            const text = await result.text();
+
+            const pre = document.createElement("pre");
+            const code = document.createElement("code");
+            pre.appendChild(code);
+            code.setText(text);
+            el.appendChild(pre);
+        }
+    };
+
+    async onload(): Promise<void> {
+        console.log('loading plugin plantuml');
+        await this.loadSettings();
+        this.addSettingTab(new PlantUMLSettingsTab(this.app, this));
+        this.registerMarkdownCodeBlockProcessor("plantuml", this.pngProcessor);
+        this.registerMarkdownCodeBlockProcessor("plantuml-ascii", this.asciiProcessor);
+        this.registerMarkdownCodeBlockProcessor("plantuml-map", this.mapProcessor);
+    }
+
+    onunload() : void {
+        console.log('unloading plugin plantuml');
+    }
+
+    async loadSettings() : Promise<void> {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() : Promise<void> {
+        await this.saveData(this.settings);
+    }
+}
+
+class PlantUMLSettingsTab extends PluginSettingTab {
+    plugin: PlantumlPlugin;
+
+    constructor(app: App, plugin: PlantumlPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const {containerEl} = this;
+
+        containerEl.empty();
+
+        new Setting(containerEl).setName("Server URL")
+            .setDesc("PlantUML Server URL")
+            .addText(text => text.setPlaceholder(DEFAULT_SETTINGS.server_url)
+                .setValue(this.plugin.settings.server_url)
+                .onChange(async (value) => {
+                        this.plugin.settings.server_url = value;
+                        await this.plugin.saveSettings();
+                    }
+                )
+            );
+        new Setting(containerEl).setName("Header")
+            .setDesc("Included at the head in every diagram. Useful for specifying a common theme (.puml file)")
+            .addTextArea(text => {
+                    text.setPlaceholder("!include https://raw.githubusercontent.com/....puml\n")
+                        .setValue(this.plugin.settings.header)
+                        .onChange(async (value) => {
+                                this.plugin.settings.header = value;
+                                await this.plugin.saveSettings();
+                            }
+                        )
+                    text.inputEl.setAttr("rows", 4);
+                    text.inputEl.addClass("settings_area")
+                }
+            );
+    }
+}
