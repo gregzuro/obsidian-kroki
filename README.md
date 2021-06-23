f3d27e29f5e7f2a23038f2c9f0b008179e76c427
Greg Zuro <greg@zuro.net>
2021-06-22 18:59:35 -0700

    first working version

--- README.md
+++ README.md
@@ -1,99 +1,31 @@
-# Obsidian PlantUML
+# Obsidian Kroki
 
-Render [PlantUML](https://plantuml.com) Diagrams in [Obsidian](https://obsidian.md)
+Render [Kroki](https://kroki.io) Diagrams in [Obsidian](https://obsidian.md).
 
-![Demonstration](https://i.imgur.com/Ueyw0SE.gif)
+This plugin uses, by default, the [Kroki](https://kroki.io) server for rendering, but specifying an alternate one via this plugin's options is encouraged.
 
-This plugin uses the [PlantUML Online Server](https://plantuml.com/server) for rendering.
+If you can't find an alternate, then you can host your own server as [described here](https://kroki.io/#install).
 
-You can also host your own server
-([Docker](https://hub.docker.com/r/plantuml/plantuml-server) /
-[JEE](https://plantuml.com/de/server) /
-[PicoWeb](https://plantuml.com/de/picoweb)) and specify its address in the settings.
+This plugin is a modified and slightly expanded version of the [obsidian-plantuml](https://github.com/joethei/obsidian-plantuml) plugin by Johannes Theiner.
 
 ## Usage
-Create a fenced codeblock using `plantuml` as the language.
-Specify your plantuml code inside.
-
-You can also use `plantuml-ascii` to generate ASCII Art, or `plantuml-map` to generate a png image
-with clickable links.
-
-Documentation on Plantuml can be found on [plantuml.com](https://plantuml.com/)
-
+Create a fenced codeblock using one of the diagram types supported by kroki as the language.
+See the [Kroki site](https://kroki.io) for a complete list of supported diagram types.
+Specify your diagram code inside the codeblock.
 
 ## Examples
 
-```yaml
-    ```plantuml
-    Bob -> Alice : hello
-    Alice -> Wonderland: hello
-    Wonderland -> next: hello
-    next -> Last: hello
-    Last -> next: hello
-    next -> Wonderland : hello
-    Wonderland -> Alice : hello
-    Alice -> Bob: hello
-    ```
-```
-results in:
-
-![](http://www.plantuml.com/plantuml/png/SyfFEhH0r-xG0iUSpEJKGmki3Yt8ICt9oUS2yo5IuVbvAQb5EObvAN1PX114ILvgHGbSKW48G08GAP_4ObGfa011NSWMe2X1IA2x6w46oUr0_y6a0000)
-
-```yaml
-    ```plantuml-ascii
-    Bob -> Alice : hello
-    Alice -> Wonderland: hello
-    Wonderland -> next: hello
-    next -> Last: hello
-    Last -> next: hello
-    next -> Wonderland : hello
-    Wonderland -> Alice : hello
-    Alice -> Bob: hello
-    ```
-```
-results in:
-```
-     ┌───┐          ┌─────┐          ┌──────────┐          ┌────┐          ┌────┐
-     │Bob│          │Alice│          │Wonderland│          │next│          │Last│
-     └─┬─┘          └──┬──┘          └────┬─────┘          └─┬──┘          └─┬──┘
-       │    hello      │                  │                  │               │   
-       │──────────────>│                  │                  │               │   
-       │               │                  │                  │               │   
-       │               │      hello       │                  │               │   
-       │               │─────────────────>│                  │               │   
-       │               │                  │                  │               │   
-       │               │                  │       hello      │               │   
-       │               │                  │ ─────────────────>               │   
-       │               │                  │                  │               │   
-       │               │                  │                  │     hello     │   
-       │               │                  │                  │ ──────────────>   
-       │               │                  │                  │               │   
-       │               │                  │                  │     hello     │   
-       │               │                  │                  │ <──────────────   
-       │               │                  │                  │               │   
-       │               │                  │       hello      │               │   
-       │               │                  │ <─────────────────               │   
-       │               │                  │                  │               │   
-       │               │      hello       │                  │               │   
-       │               │<─────────────────│                  │               │   
-       │               │                  │                  │               │   
-       │    hello      │                  │                  │               │   
-       │<──────────────│                  │                  │               │   
-     ┌─┴─┐          ┌──┴──┐          ┌────┴─────┐          ┌─┴──┐          ┌─┴──┐
-     │Bob│          │Alice│          │Wonderland│          │next│          │Last│
-     └───┘          └─────┘          └──────────┘          └────┘          └────┘
-```
-
+See [kroki-test.md](kroki-test.md) for examples of each of the currently supported diagram types.
 
 ## Installation
 
-### Inside Obsidian
+### Inside Obsidian (TBD)
 
-`Settings > Third-party plugins > Community Plugins > Browse` and search for `PlantUML`.
+`Settings > Third-party plugins > Community Plugins > Browse` and search for `Kroki`.
 
 ### Manually installing the plugin
 
 - Clone this repo
 - `npm i` or `yarn` to install dependencies
 - `npm run build`  
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/obsidian-plantuml/`.
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/obsidian-kroki/`.
--- main.ts
+++ main.ts
@@ -1,88 +1,132 @@
 import {App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
-import * as plantuml from 'plantuml-encoder'
+import * as pako from 'pako';
 
-interface PlantUMLSettings {
+interface KrokiSettings {
     server_url: string,
     header: string;
 }
 
-const DEFAULT_SETTINGS: PlantUMLSettings = {
-    server_url: 'https://www.plantuml.com/plantuml',
+const DEFAULT_SETTINGS: KrokiSettings = {
+    server_url: 'https://kroki.io/',
     header: ''
 }
 
-export default class PlantumlPlugin extends Plugin {
-    settings: PlantUMLSettings;
+export default class KrokiPlugin extends Plugin {
+    settings: KrokiSettings;
 
-    pngProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+    svgProcessor = async (diagType: string, source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
         const dest = document.createElement('img');
-        const prefix = this.settings.server_url + "/png/";
+        const prefix = this.settings.server_url + diagType + "/svg/";
         source = source.replace(/&nbsp;/gi, " ");
 
-        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
+        // https://docs.kroki.io/kroki/setup/encode-diagram/#nodejs
+        const data = Buffer.from(source, 'utf8');
+        const compressed = pako.deflate(data, { level: 9 });
+        const encoded = Buffer.from(compressed)
+          .toString('base64')
+          .replace(/\+/g, '-').replace(/\//g, '_');
 
         dest.src = prefix + encoded;
+        console.log(dest.src)
+
 
         el.appendChild(dest);
     };
 
-    mapProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
-        const dest = document.createElement('div');
-        let prefix = this.settings.server_url + "/png/";
-        source = source.replace(/&nbsp;/gi, " ");
-
-        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
-
-        const img = document.createElement("img");
-        img.src = prefix + encoded;
-        img.useMap = "#" + encoded;
-
-        prefix = this.settings.server_url + "/map/";
-
-        const result = await fetch(prefix + encoded, {
-            method: "GET"
-        });
-
-        if(result.ok) {
-            dest.innerHTML = await result.text();
-            dest.children[0].setAttr("name", encoded);
-        }
-        dest.appendChild(img);
-        el.appendChild(dest);
-    };
-
-    asciiProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
-        const prefix = this.settings.server_url + "/txt/";
-        source = source.replace(/&nbsp;/gi, " ");
-
-        const encoded = plantuml.encode(this.settings.header + "\r\n" + source);
-        const result = await fetch(prefix + encoded, {
-            method: "GET"
-        });
-
-        if(result.ok) {
-            const text = await result.text();
-
-            const pre = document.createElement("pre");
-            const code = document.createElement("code");
-            pre.appendChild(code);
-            code.setText(text);
-            el.appendChild(pre);
-        }
-    };
+    blockdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("blockdiag", source, el, _)
+    }
+    bpmnProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("bpmn", source, el, _)
+    }
+    bytefieldProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("bytefield", source, el, _)
+    }
+    seqdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("seqdiag", source, el, _)
+    }
+    actdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("actdiag", source, el, _)
+    }
+    nwdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("nwdiag", source, el, _)
+    }
+    packetdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("packetdiag", source, el, _)
+    }
+    rackdiagProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("rackdiag", source, el, _)
+    }
+    c4plantumlProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("c4plantuml", source, el, _)
+    }
+    ditaaProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("ditaa", source, el, _)
+    }
+    erdProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("erd", source, el, _)
+    }
+    excalidrawProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("excalidraw", source, el, _)
+    }
+    graphvizProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("graphviz", source, el, _)
+    }
+    mermaidProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("mermaid", source, el, _)
+    }
+    nomnomlProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("nomnoml", source, el, _)
+    }
+    pikchrProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("pikchr", source, el, _)
+    }
+    plantumlProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("plantuml", source, el, _)
+    }
+    svgbobProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("svgbob", source, el, _)
+    }
+    vegaProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("vega", source, el, _)
+    }
+    vegaliteProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("vegalite", source, el, _)
+    }
+    wavedromProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+        this.pngProcessor("wavedrom", source, el, _)
+    }
 
     async onload(): Promise<void> {
-        console.log('loading plugin plantuml');
+        console.log('loading plugin kroki');
         await this.loadSettings();
-        this.addSettingTab(new PlantUMLSettingsTab(this.app, this));
-        this.registerMarkdownCodeBlockProcessor("plantuml", this.pngProcessor);
-        this.registerMarkdownCodeBlockProcessor("plantuml-ascii", this.asciiProcessor);
-        this.registerMarkdownCodeBlockProcessor("plantuml-map", this.mapProcessor);
+        this.addSettingTab(new KrokiSettingsTab(this.app, this));
+        this.registerMarkdownCodeBlockProcessor("blockdiag", this.blockdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("bpmn", this.bpmnProcessor);
+        this.registerMarkdownCodeBlockProcessor("bytefield", this.bytefieldProcessor);
+        this.registerMarkdownCodeBlockProcessor("seqdiag", this.seqdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("actdiag", this.actdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("nwdiag", this.nwdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("packetdiag", this.packetdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("rackdiag", this.rackdiagProcessor);
+        this.registerMarkdownCodeBlockProcessor("c4plantuml", this.c4plantumlProcessor);
+        this.registerMarkdownCodeBlockProcessor("ditaa", this.ditaaProcessor);
+        this.registerMarkdownCodeBlockProcessor("erd", this.erdProcessor);
+        this.registerMarkdownCodeBlockProcessor("excalidraw", this.excalidrawProcessor);
+        this.registerMarkdownCodeBlockProcessor("graphviz", this.graphvizProcessor);
+        this.registerMarkdownCodeBlockProcessor("mermaid", this.mermaidProcessor);
+        this.registerMarkdownCodeBlockProcessor("nomnoml", this.nomnomlProcessor);
+        this.registerMarkdownCodeBlockProcessor("pikchr", this.pikchrProcessor);
+        this.registerMarkdownCodeBlockProcessor("plantuml", this.plantumlProcessor);
+        this.registerMarkdownCodeBlockProcessor("svgbob", this.svgbobProcessor);
+        this.registerMarkdownCodeBlockProcessor("vega", this.vegaProcessor);
+        this.registerMarkdownCodeBlockProcessor("vegalite", this.vegaliteProcessor);
+        this.registerMarkdownCodeBlockProcessor("wavedrom", this.wavedromProcessor);
     }
 
     onunload() : void {
-        console.log('unloading plugin plantuml');
+        console.log('unloading plugin kroki');
     }
 
     async loadSettings() : Promise<void> {
@@ -94,10 +138,10 @@ export default class PlantumlPlugin extends Plugin {
     }
 }
 
-class PlantUMLSettingsTab extends PluginSettingTab {
-    plugin: PlantumlPlugin;
+class KrokiSettingsTab extends PluginSettingTab {
+    plugin: KrokiPlugin;
 
-    constructor(app: App, plugin: PlantumlPlugin) {
+    constructor(app: App, plugin: KrokiPlugin) {
         super(app, plugin);
         this.plugin = plugin;
     }
@@ -108,7 +152,7 @@ class PlantUMLSettingsTab extends PluginSettingTab {
         containerEl.empty();
 
         new Setting(containerEl).setName("Server URL")
-            .setDesc("PlantUML Server URL")
+            .setDesc("Kroki Server URL")
             .addText(text => text.setPlaceholder(DEFAULT_SETTINGS.server_url)
                 .setValue(this.plugin.settings.server_url)
                 .onChange(async (value) => {
